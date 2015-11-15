e3799896ccdb382069a5d2ca493bca2b66744d11
Dorian Karter <dkarter@gmail.com>
2015-11-15 14:21:02 -0600

    Implement socket io basic server

---
+++ server.js
@@ -0,0 +1,27 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = 3000;
+
+app.get('/', (req, res) => {
+  res.sendfile('index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log(`a user connected! id: ${socket.id}`);
+  console.log("Sending clipboard to client: CLIPBOARD FROM SERVER");
+  io.emit('clipboardPublished', 'CLIPBOARD FROM SERVER');
+
+  socket.on('clipboardPublished', (data) => {
+    console.log(`Clipboard from client: ${data}`);
+  });
+});
+
+io.on('disconnect', (socket) => {
+  console.log('user disconnected :(');
+});
+
+http.listen(port, () => {
+  console.log(`Listening on ${port}`)
+});
+
